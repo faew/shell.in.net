6ba36476735db923df8424afa8ece848aab5b9d6
faew <faew@ru5.net>
2021-03-05 17:44:13 +0400

    Add postgres backup

---
+++ backup/pgsql.sh
@@ -0,0 +1,15 @@
+# Copyright © 2021 Shell.in.net All rights reserved.
+# Licensed under the GNU Lesser General Public License v3.0
+
+backup_dir="/var/backup/pgsql"
+cdate=`date +%F`
+tdaydir="$backup_dir/$cdate"
+
+mkdir -p $tdaydir
+for i in `su -l postgres -c  'psql -l -t' | grep -v template | awk '{ print $1 }' | grep -v '|' | awk 'NF'`
+do
+    su -l postgres -c "pg_dump -O $i" > $tdaydir/$i.sql
+done
+cd $backup_dir
+tar --remove-files -cPjf "$backup_dir/mysql_$cdate.tar.bz2" "$cdate"
+rm -f $backup_dir/mysql_`date -d "14 days ago" "+%F"`.tar.bz2
